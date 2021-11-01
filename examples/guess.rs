8973d069d5dbe001e99ee85dcaf6c59ce21f622e
techzealot <techzealot@foxmail.com>
2021-11-01 16:00:39 +0800

    move guess number game to examples

---
+++ examples/guess.rs
@@ -0,0 +1,27 @@
+use std::cmp::Ordering;
+use std::io::stdin;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("please input your guess.");
+        let mut guess = String::new();
+        stdin().read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed:{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too large"),
+            Ordering::Equal => {
+                println!("you get it");
+                break;
+            }
+        }
+    }
+}
